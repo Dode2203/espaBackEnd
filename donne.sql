0af83ece094d4e43a77bad22dadf511fb38d2019
Dode2203 <153656111+Dode2203@users.noreply.github.com>
2025-12-17 12:10:48 +0300

    user

--- donne.sql
+++ donne.sql
@@ -1,3 +1,14 @@
+INSERT INTO Utilisateur (id, email, password, prenom, nom, is_active, is_admin)
+VALUES (
+    4,
+    'admin@gmail.com',
+    '$2y$10$Djns8FgsL.xk2GBACEtJh.Hs1civTyvdGQ9s6gqbSgDN81QkOHvTi',
+    'admin',
+    'admin',
+    1,
+    1
+);
+
 INSERT INTO role (id, name) VALUES (1, 'Admin');
 INSERT INTO role (id, name) VALUES (2, 'Utilisateur');
 
