7e39afd53817b313487a932c9565372648084081
Dode2203 <153656111+Dode2203@users.noreply.github.com>
2025-12-17 12:20:52 +0300

    Update donne.sql

--- donne.sql
+++ donne.sql
@@ -1,13 +1,3 @@
-INSERT INTO Utilisateur (id, email, password, prenom, nom, is_active, is_admin)
-VALUES (
-    4,
-    'admin@gmail.com',
-    '$2y$10$Djns8FgsL.xk2GBACEtJh.Hs1civTyvdGQ9s6gqbSgDN81QkOHvTi',
-    'admin',
-    'admin',
-    1,
-    1
-);
 
 INSERT INTO role (id, name) VALUES (1, 'Admin');
 INSERT INTO role (id, name) VALUES (2, 'Utilisateur');
@@ -19,6 +9,16 @@ INSERT INTO Status (id, name) VALUES (1, 'Actif');
 INSERT INTO Status (id, name) VALUES (2, 'Inactif');
 
 
-UPDATE utilisateur SET status_id = 2;
+INSERT INTO Utilisateur (id, email, password, prenom, nom, is_active, is_admin)
+VALUES (
+    4,
+    'admin@gmail.com',
+    '$2y$10$Djns8FgsL.xk2GBACEtJh.Hs1civTyvdGQ9s6gqbSgDN81QkOHvTi',
+    'admin',
+    'admin',
+    1,
+    1
+);
+
 
 
